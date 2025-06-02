a95ab18c18bc332e2fbc19ec5a971c33b2e98693
xxl <532981394@qq.com>
2025-06-02 14:41:56 +0800

    Initial Commit

---
+++ ScreanLight/ScreanLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreanLightApp.swift
+//  ScreanLight
+//
+//  Created by 徐晓龙 on 2025/6/2.
+//
+
+import SwiftUI
+
+@main
+struct ScreanLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScreanLightTests/ScreanLightTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScreanLightTests.swift
+//  ScreanLightTests
+//
+//  Created by 徐晓龙 on 2025/6/2.
+//
+
+import Testing
+@testable import ScreanLight
+
+struct ScreanLightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
